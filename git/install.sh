3492a299e96de4168c116fdbfc6c61d8d0c1f589
Tom Pollak <tompollak1000@gmail.com>
2021-01-31 11:06:06 +0000

    update

--- git/install.sh
+++ git/install.sh
@@ -4,17 +4,12 @@ DOTFILES=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/../" && pwd -P)
 
 echo "[ LINKING ] :: Git >> config"
 
-ln -sf "$DOTFILES/git/gitconfig" $HOME/.gitconfig.local
+ln -sf "$DOTFILES/git/gitconfig.local" $HOME/.gitconfig.local
+ln -sf "$DOTFILES/git/gitconfig" $HOME/.gitconfig
 
 git config --global include.path $HOME/.gitconfig.local
 
 # If hub is found, use https: instead of git: or ssh::
 if command -v hub >/dev/null 2>&1; then
     git config --global hub.protocol https
-fi
-
-# use vscode as mergetool
-if command -v code >/dev/null 2>&1; then
-	git config --global merge.tool vscode
-	git config --global mergetool.vscode.cmd "code --wait $MERGED"
 fi
