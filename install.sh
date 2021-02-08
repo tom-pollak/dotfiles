2059b0c3ed34fb295bb540db5342721852ce3911
Tom Pollak <tompollak1000@gmail.com>
2021-02-08 15:09:15 +0000

    update

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ for dictionary in `ls`; do
 	chmod +x $dictionary/install.sh
 	$dictionary/install.sh
 done
+tmux source-file .tmux.conf
--- install_tools.sh
+++ install_tools.sh
@@ -1,7 +1,14 @@
-sudo apt install -y fd-find build-essential curl file git bat neovim-nightly tmux
+sudo apt install -y fd-find build-essential curl file git bat tmux python3-neovim python-neovim fuse zsh python3 exa python3-pip
+sudo curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage > /usr/bin/nvim && sudo chmod +x /usr/bin/nvim
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> $HOME/.zprofile
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
-brew install fzf && $(brew --prefix)/opt/fzf/install
+brew install fzf starship && $(brew --prefix)/opt/fzf/install
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+sudo apt install -y nodejs npm
