750db99b58ce3527a729b5f34a8ead509a919c86
Tom Pollak <tompollak1000@gmail.com>
2021-02-08 15:42:18 +0000

    update

--- install_tools.sh
+++ install_tools.sh
@@ -12,3 +12,4 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 sudo apt install -y nodejs npm
+pip3 install neovim jedi-language-server
--- nvim/install.sh
+++ nvim/install.sh
@@ -7,5 +7,4 @@ for f in `find . -regex ".*\.vim$\|.*\.json$"`; do
     ln -sf $DOTFILES/nvim/$f $HOME/.config/nvim/$f
 done
 
-rm -rf $HOME/.vimspector.json
 ln -sf $DOTFILES/nvim/vimspector.json $HOME/.vimspector.json
