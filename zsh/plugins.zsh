8cc61ade8c10fc9037d65b1e13ef9f64ca1487f4
Tom Pollak <tompollak1000@gmail.com>
2022-01-01 17:33:38 +0000

    update

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,10 +1,10 @@
-zinit light zsh-users/zsh-completions
-zinit light zsh-users/zsh-autosuggestions
-zinit light zsh-users/zsh-syntax-highlighting
+#zinit light zsh-users/zsh-completions
+#zinit light zsh-users/zsh-autosuggestions
+#zinit light zsh-users/zsh-syntax-highlighting
 zplugin ice as "program" pick "bin/git-dsf"
 zplugin light zdharma/zsh-diff-so-fancy
 
 
-plugins=(git virtualenv alias-tips docker vi-mode tmux taskwarrior transfer poetry)
+plugins=(zsh-completions zsh-autosuggestions zsh-syntax-highlighting git virtualenv alias-tips docker vi-mode tmux taskwarrior transfer poetry)
 source ~/.oh-my-zsh/oh-my-zsh.sh
 autoload -U compinit && compinit
