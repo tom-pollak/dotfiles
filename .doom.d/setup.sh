6bb2d136f56832e89842db75ec208e6ca5e89ef9
Tom Pollak <tompollak1000@gmail.com>
2022-02-10 02:47:22 +0000

    update b4 config fish changes

--- .doom.d/setup.sh
+++ .doom.d/setup.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 cd /tmp
+curl -o "hunspell-en-custom.zip" 'http://app.aspell.net/create?max_size=80&spelling=GBs&spelling=AU&max_variant=0&diacritic=keep&special=hacker&special=roman-numerals&encoding=utf-8&format=inline&download=hunspell'
+unzip "hunspell-en-custom.zip"
+
+sudo chown root:root en-custom.*
+sudo mv en-custom.{aff,dic} /usr/share/myspell/
+
+cd /tmp
 curl -o "aspell6-en-custom.tar.bz2" 'http://app.aspell.net/create?max_size=80&spelling=GBs&spelling=AU&max_variant=0&diacritic=keep&special=hacker&special=roman-numerals&encoding=utf-8&format=inline&download=aspell'
 tar -xjf "aspell6-en-custom.tar.bz2"
 
