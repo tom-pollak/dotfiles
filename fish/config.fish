3b783ae55c7f97a6db91cc6d73fd9026ebfe0ed1
Tom Pollak <tompollak1000@gmail.com>
2022-06-01 19:27:23 +0100

    update

--- fish/config.fish
+++ fish/config.fish
@@ -5,15 +5,18 @@ end
 set -gx PATH $HOME/.local/bin:/opt/apache-maven-3.6.3/bin:$HOME/.emacs.d/bin:/home/linuxbrew/.linuxbrew/bin:$HOME/.poetry/bin:/opt/homebrew/bin:/Users/tom/opt/anaconda3/bin:/usr/local/bin $PATH
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set -gx PAGER less
-set -gx EDITOR nvim
+set -gx EDITOR lvim
+set -gx GIT_EDITOR lvim
 set -gx VIMRC $HOME/.config/nvim/init.vim
 set -gx DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
 
 
 set -gx LIBGL_ALWAYS_INDIRECT 1
 
+set -gx PROJECT_PATHS ~/projects
+set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=$(brew --prefix openssl@1.1)"
 set -gx GRADLE_HOME /usr/share/gradle
-set -gx JAVA_HOME /opt/homebrew/Cellar/openjdk/17.0.2/libexec/openjdk.jdk/Contents/Home
+set -gx JAVA_HOME /Users/tom/Library/Java/JavaVirtualMachines/temurin-17.0.2/Contents/Home
 set -gx JDTLS_CONFIG /usr/lib/jvm/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/config_linux
 set -gx WORKSPACE $HOME/projects
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
@@ -33,6 +36,7 @@ alias untar='tar -zxvf '
 alias ipe="curl -w '\n' ipinfo.io/ip"
 alias sync="sudo ntpdate pool.ntp.org"
 alias c="clear"
+alias dghci="TERM=dumb ghci"
 
 bind -M insert \co 'nvim $(fzf)'
 bind -M insert \cj forward-char
@@ -67,8 +71,13 @@ set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
 
 set _CONDA_ROOT "/Users/tom/opt/anaconda3"
 
+set -gx LDFLAGS "-L/opt/homebrew/opt/ruby/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/ruby/include"
+set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/ruby/lib/pkgconfig"
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
+
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
