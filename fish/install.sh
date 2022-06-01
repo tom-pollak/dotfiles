dd8cd46123ca20ae457aef37ccddb1c02efa9f1a
Tom Pollak <tompollak1000@gmail.com>
2022-06-01 19:27:23 +0100

    update

--- fish/install.sh
+++ fish/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 source ../install_module.sh
-install_module "fish" "fish/config.fish" "$HOME/.config/fish/config.fish"
+install_module "fish" "fish/config.fish" "$HOME/.config/fish/config.fish" true
+install_module "fish" "fish/fisher-plugins" "$HOME/.config/fish/fisher-plugins" true
+install_module "fish" "fish/fish_variables" "$HOME/.config/fish/fish_variables" true
 fish
---
+++ obsidian/obsidian.vimrc
@@ -0,0 +1,17 @@
+nmap j gj
+nmap k gk
+
+imap jk <esc>
+
+nmap H ^
+nmap L $
+
+" Yank to system clipboard
+set clipboard=unnamed
+
+" Go back and forward with Ctrl+O and Ctrl+I
+" (make sure to remove default Obsidian shortcuts for these to work)
+exmap back obcommand app:go-back
+nmap <C-o> :back
+exmap forward obcommand app:go-forward
+nmap <C-i> :forward
