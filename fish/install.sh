58a452bb404ae5bf07c831c48cf4f11c8476a4d9
Tom Pollak <tompollak1000@gmail.com>
2022-06-15 19:43:38 +0100

    updated fish config

--- fish/install.sh
+++ fish/install.sh
@@ -4,4 +4,5 @@ source ../install_module.sh
 install_module "fish" "fish/config.fish" "$HOME/.config/fish/config.fish" true
 install_module "fish" "fish/fisher-plugins" "$HOME/.config/fish/fisher-plugins" true
 install_module "fish" "fish/fish_variables" "$HOME/.config/fish/fish_variables" true
+install_module "fish" "fish/fish_plugins" "$HOME/.config/fish/fish_plugins" true
 fish
