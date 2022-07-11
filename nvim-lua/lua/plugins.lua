0ab5a53608306045c3d5f9cbdb515af151d28e22
Thomas Pollak <tompollak1000@gmail.com>
2022-07-11 13:08:22 +0100

    update

--- nvim-lua/lua/plugins.lua
+++ nvim-lua/lua/plugins.lua
@@ -1,7 +1,4 @@
--- require('cmp-config')
--- Plugin specific mappings
-
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 
 -- Telescope
 vim.api.nvim_set_keymap("n", "<c-p>", "<CMD>lua require'telescope-config'.project_files()<CR>", opts)
@@ -12,81 +9,68 @@ vim.api.nvim_set_keymap("n", "<c-c>", "<CMD>lua require'telescope-config'.live_g
 vim.api.nvim_set_keymap("n", "<leader>cd", "<CMD>lua require'telescope-config'.dotfiles()<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>cc", "<CMD>lua require'telescope-config'.vim_config()<CR>", opts)
 
-vim.api.nvim_set_keymap("n", "<c-;>", "<CMD>QNext<CR>", opts)
-vim.api.nvim_set_keymap("n", "<c-,>", "<CMD>QPrev<CR>", opts)
--- jump to the next item, skipping the groups
--- vim.api.nvim_set_keymap("n", "<c-;>", ':lua require("trouble").next({skip_groups = true, jump = true})<CR>', {silent=true})
--- vim.api.nvim_set_keymap("n", "<c-,>", ':lua require("trouble").previous({skip_groups = true, jump = true})<CR>', {silent=true})
+-- QF Helper
+vim.api.nvim_set_keymap("n", "<leader>;", "<CMD>QNext<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>,", "<CMD>QPrev<CR>", opts)
 
 vim.api.nvim_set_keymap("n", "<leader>k", "<CMD>QFToggle!<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>l", "<CMD>LLToggle!<CR>", opts)
 
 
+-- Neogit
 vim.api.nvim_set_keymap("n", "<leader>j", "<CMD>Neogit<CR>", opts)
 
--- Pear tree
 
-vim.g.pear_tree_repeatable_expand = 0
-vim.g.pear_tree_map_special_keys = 0
+-- Undotree
+vim.api.nvim_set_keymap("n", "<leader>u", "<CMD>UndotreeToggle<CR>", opts)
 
 
--- Nvim Tree
-
--- vim.api.nvim_set_keymap("n", "<c-n>", "<CMD>lua require'tree-config'.toggle_replace()<CR>", opts)
+-- Ranger
 vim.api.nvim_set_keymap("n", "<c-n>", "<CMD>Ranger<CR>", opts)
-vim.g.ranger_replace_netrw = 1
 
 
--- Treesitter
+-- TMUX Navigator
+vim.api.nvim_set_keymap("n", "<A-h>", "<CMD>TmuxNavigateLeft<CR>", opts)
+vim.api.nvim_set_keymap("n", "<A-l>", "<CMD>TmuxNavigateRight<CR>", opts)
+vim.api.nvim_set_keymap("n", "<A-k>", "<CMD>TmuxNavigateUp<CR>", opts)
+vim.api.nvim_set_keymap("n", "<A-j>", "<CMD>TmuxNavigateDown<CR>", opts)
 
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "all",
-
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  rainbow = { -- dosen't work
-      enable = true,
-      extended_mode = true,
-      max_file_lines = nil,
-  },
-  context_commentstring = {
-      enable = true
-  }
-}
-
-
+-------------------------------------------------------------------------------
 -- LSP
--- Keymaps
-vim.keymap.set('n', 'gh', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', 'gj', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', 'gk', vim.diagnostic.goto_next, opts)
+-------------------------------------------------------------------------------
 
+-- Keymaps
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- Enable completion triggered by <c-x><c-o>
+    -- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  -- vim.keymap.set('n', '<leader>k', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<leader>q', vim.lsp.buf.formatting, bufopts)
+    -- Mappings. (vim.lsp.*)
+
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', 'gj', vim.diagnostic.goto_prev, bufnr)
+    vim.keymap.set('n', 'gk', vim.diagnostic.goto_next, bufnr)
+
+    vim.keymap.set('n', 'gh', vim.diagnostic.open_float, bufnr)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    -- vim.keymap.set('n', '<leader>k', vim.lsp.buf.signature_help, bufopts)
+
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', '<leader>q', vim.lsp.buf.formatting, bufopts)
 end
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -99,7 +83,7 @@ lspconfig.sumneko_lua.setup {
         Lua = {
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
-                globals = {'vim', 'use'},
+                globals = { 'vim', 'use' },
             },
         },
     },
@@ -128,29 +112,7 @@ lspconfig.pylsp.setup {
 }
 
 require("clangd_extensions").setup()
-lspconfig.clangd.setup{
+lspconfig.clangd.setup {
     on_attach = on_attach,
     capabilities = capabilities
 }
-
-
--- Trouble
---
--- vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
---   {silent = true, noremap = true}
--- )
--- vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>",
---   {silent = true, noremap = true}
--- )
--- vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>",
---   {silent = true, noremap = true}
--- )
--- vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
---   {silent = true, noremap = true}
--- )
--- vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
---   {silent = true, noremap = true}
--- )
--- vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>",
---   {silent = true, noremap = true}
--- )
