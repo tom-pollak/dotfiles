c83c33c7c51f60012176f3d84e5aed6f3fe2b9d7
Thomas Pollak <tpollak@meraki.com>
2022-07-24 00:43:48 +0100

    update

--- nvim-lua/lua/colors.lua
+++ nvim-lua/lua/colors.lua
@@ -1,7 +1,12 @@
-local ok, _ = pcall(vim.cmd, 'colorscheme PaperColor')
+local ok, _ = pcall(vim.cmd, 'colorscheme github_dark_default')
+-- ok = false
 if not ok then
     vim.cmd 'colorscheme default' -- if the above fails, then use default
 end
 
-vim.o.background = 'dark'
-vim.cmd[[highlight LineNr ctermfg=darkyellow]]
+-- vim.highlight.create('CursorLineNr', {cterm='underline' ctermfg='Yellow' guifg='Yellow'}, false)
+-- vim.o.background = 'dark'
+-- vim.wo.colorcolumn = '80'
+-- vim.highlight.create('ColorColumn', {ctermbg=238, guibg="grey"}, false)
+-- vim.cmd 'highlight LineNr ctermfg=darkyellow'
+-- vim.cmd 'highlight colorcolumn guibg=grey'
