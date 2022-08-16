c71717ceed0deb0404699a789119ae897c16bcae
Thomas Pollak <tompollak1000@gmail.com>
2022-08-16 01:11:02 +0100

    Reformatted files, clearer packer sections

--- nvim-lua/init.lua
+++ nvim-lua/init.lua
@@ -1,11 +1,3 @@
-
--- 10ms
 require('core/base')
 require('core/mappings')
 require('packer_init')
--- require('plugins/plugins')
----------------------------------------
--- -- 35ms
--- require('plugins/telescope')
--- -- 20ms
--- require('plugins/lsp')
