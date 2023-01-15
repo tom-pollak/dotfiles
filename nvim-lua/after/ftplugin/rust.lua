61f405e76ed88b026aba10423ac7983edc036817
Thomas Pollak <tompollak1000@gmail.com>
2023-01-15 20:35:35 +0000

    deleted old config

--- nvim-lua/after/ftplugin/rust.lua
+++
@@ -1,7 +0,0 @@
-local format = function()
-    vim.cmd [[ update ]]
-    vim.fn.system("rustfmt " .. vim.fn.expand('%'))
-    vim.cmd [[ edit | TSBufEnable highlight ]]
-    vim.cmd [[ write ]]
-    print("File formatted.")
-end
