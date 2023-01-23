6c40ee16e92bb5636074dd8c2f058573af320281
Thomas Pollak <tompollak1000@gmail.com>
2023-01-23 15:20:10 +0000

    updated delta

--- git/install.sh
+++ git/install.sh
@@ -2,9 +2,6 @@
 
 source ../install_module.sh
 install_module "git" "git/gitconfig" "$HOME/.gitconfig"
-install_module "git" "git/gitconfig.local" "$HOME/.gitconfig.local"
-
-git config --global include.path $HOME/.gitconfig.local
 
 # If hub is found, use https: instead of git: or ssh::
 if command -v hub >/dev/null 2>&1; then
