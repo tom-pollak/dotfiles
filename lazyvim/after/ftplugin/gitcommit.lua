bd53fc529bf513569797486fb94213a9b1b72939
Tom Pollak <tompollak1000@gmail.com>
2024-06-11 19:40:50 +0100

    added all gitsigns shortcuts

---
+++ lazyvim/after/ftplugin/gitcommit.lua
@@ -0,0 +1,13 @@
+local set = vim.opt_local
+
+set.wrap = true
+set.linebreak = true
+
+set.tabstop = 2
+set.shiftwidth = 2
+set.softtabstop = 2
+
+vim.wo.colorcolumn = '72'
+vim.g.tw=72
+
+vim.cmd[[filetype indent plugin on]]
