80399fcd56d48e487b6625b13e37ffa5dbe969ff
Tom Pollak <tompollak100@gmail.com>
2024-06-11 19:50:16 +0100

    deleted random comments

--- lazyvim/init.lua
+++ lazyvim/init.lua
@@ -289,10 +289,7 @@ require("lazy").setup({
             "LazyGitFilter",
             "LazyGitFilterCurrentFile",
         },
-        -- optional for floating window border decoration
         dependencies = { "nvim-lua/plenary.nvim" },
-        -- setting the keybinding for LazyGit with 'keys' is recommended in
-        -- order to load the plugin when the command is run for the first time
         keys = { { "<leader>j", "<cmd>LazyGit<cr>", desc = "LazyGit" } }
     }
 })
