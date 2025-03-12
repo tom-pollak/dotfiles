1cc1364b24e9da87283eef1b5312e583bfe40dcf
Tom Pollak <tompollak1000@gmail.com>
2025-03-12 08:24:55 +0000

    curosr install

---
+++ cursor/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ../install_module.sh
+install_module "cursor" "cursor/settings.json" "$HOME/Library/Application Support/Cursor/User/settings.json"
+install_module "cursor" "cursor/keybindings.json" "$HOME/Library/Application Support/Cursor/User/keybindings.json"
